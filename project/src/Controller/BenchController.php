8ba810c56c9f5ba20896da5d2cbc28bdda247065
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2025-11-30 16:52:22 +0200

    update BenchController

--- project/src/Controller/BenchController.php
+++ project/src/Controller/BenchController.php
@@ -10,18 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BenchController extends AbstractController
 {
-    /**
-     * A benchmark route for the Health check scenario
-     */
     #[Route('/bench/health-check', name: 'bench_health_check', methods: ['GET'])]
     public function health(): Response
     {
         return new Response(content: 'OK', status: Response::HTTP_OK);
     }
 
-    /**
-     * A benchmark route for the I/O scenario
-     */
     #[Route('/bench/io', name: 'bench_io', methods: ['GET'])]
     public function io(#[MapQueryParameter] int $ms = 5): JsonResponse
     {
